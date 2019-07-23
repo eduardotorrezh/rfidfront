e3d873c9eaf172556cc960ffadeae1df23b09c6e
Edddduardo <173631@ids.upchiapas.edu.mx>
2019-07-23 01:35:29 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import Router from 'vue-router'
+import routes from './routes';
+
+
+
+const router = new Router({mode: 'history', routes});
+
+Vue.use(Vuetify,Router,)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import plist from './components/profesor/list.vue';
+
+const routes = [
+    {path: '/plist',
+      component: plist,
+      name: 'Lista de profesores'
+    }
+];
+
+export default routes;
